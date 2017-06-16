acc466dda079f816a22c0b76e1ee336bd1d2dd44
Oksana Leskiv <oksana.leskiv@gmail.com>
2017-06-16 16:30:09 +0300

    added new files

--- js/custom.js
+++ js/custom.js
@@ -8,3 +8,71 @@ $(function(){
 new WOW().init();
 
 });
+
+/*============================================
+     Work
+============================================*/
+$(function(){
+
+$("#work").magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image',
+
+  gallery:{
+    enabled:true
+  }
+});
+
+});
+
+/*============================================
+     Team
+============================================*/
+$(function(){
+  $("#team-members").owlCarousel({
+    items: 3,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true
+  });
+});
+
+
+/*============================================
+     Testimonials
+============================================*/
+$(function(){
+  $("#customers-testimonials").owlCarousel({
+    items: 1,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true
+  });
+});
+
+
+/*============================================
+     Stats
+============================================*/
+$(function(){
+  $('.counter').counterUp({
+                delay: 10,
+                time: 2000
+            });
+});
+
+/*============================================
+     Clients
+============================================*/
+$(function(){
+  $("#clients-list").owlCarousel({
+    items: 6,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true
+  });
+});
+
